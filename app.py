a83f6ec2ac26812c342b7aea8fd095a6ea4638db
waxx567 <waynem567@gmail.com>
2022-12-30 19:36:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -11,15 +11,16 @@ CLIENT = "gaslitcatfish web designs"
 CLIENT_1 = "gaslit"
 CLIENT_2 = "catfish"
 CLIENT_3 = "designs"
-
-BUILDER = "fivefiftyfive"
-BUILDER_1 = "ltd"
+BUILDER = "fivefiftyfive ltd"
+BUILDER_1 = "five"
+BUILDER_2 = "fifty"
+BUILDER_3 = "ltd"
 YEAR = "2022"
 
 
 @app.route("/")
 def home():
-    return render_template("index.html", client=CLIENT, client_1=CLIENT_1, client_2=CLIENT_2, year=YEAR, builder=BUILDER, builder_1=BUILDER_1)
+    return render_template("index.html", client=CLIENT, client_1=CLIENT_1, client_2=CLIENT_2, builder=BUILDER, builder_1=BUILDER_1, builder_2=BUILDER_2, builder_3=BUILDER_3, year=YEAR)
 
 
 if __name__ == "__main__":
