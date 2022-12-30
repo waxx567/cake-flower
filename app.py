266094f0019df9c367661045be5a29797ede886c
waxx567 <waynem567@gmail.com>
2022-12-30 17:46:20 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ from flask import Flask, redirect, render_template, request
 
 connection = sqlite3.connect("users.db")
 
+cursor = connection.cursor()
+cursor.execute("")
+
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
