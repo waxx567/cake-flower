764dff8daafa87bec2584268098726f3605d90ff
waxx567 <waynem567@gmail.com>
2022-12-30 12:33:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
 from flask import Flask, redirect, render_template, request, session, url_for, flash
 from datetime import timedelta
 from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
