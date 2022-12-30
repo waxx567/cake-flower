2f5d1f483f69ee5bf70e2d28882c24b6677e39a5
waxx567 <waynem567@gmail.com>
2022-12-30 12:40:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,3 +3,8 @@ from datetime import timedelta
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
