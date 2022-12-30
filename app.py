25833707c15aceb3d958a9f11dc1184e21356cc8
waxx567 <waynem567@gmail.com>
2022-12-30 18:01:24 +0200

    same

--- app.py
+++ app.py
@@ -4,7 +4,10 @@ from flask import Flask, redirect, render_template, request
 connection = sqlite3.connect("users.db")
 
 cursor = connection.cursor()
-cursor.execute("")
+cursor.execute(
+    "INSERT INTO users VALUES ('waxx567', 'waynem567@gmail.com', '4 Shaw St', 'Shit Towne', '6789', '+27791887713')")
+
+    # FORGOT THE USER'S ACTUAL NAME/S 
 
 if __name__ == "__main__":
     with app.app_context():
