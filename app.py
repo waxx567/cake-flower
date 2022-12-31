2b111fd181abe8a83f86a0bae45e76b4864c574e
waxx567 <waynem567@gmail.com>
2022-12-31 13:31:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ c = conn.cursor()
 # Query database
 c.execute("SELECT * FROM test")
 
-# Prints the first element of the first tuple when input of python app.py
-print(c.fetchone()[0])
+# Prints the second element of the first tuple when input of python app.py
+print(c.fetchone()[1])
 
 # Commit command
 conn.commit()
