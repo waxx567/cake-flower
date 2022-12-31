885d0e42b05b5e9836ac1b93f1b38fafc0d1a21d
waxx567 <waynem567@gmail.com>
2022-12-31 07:08:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__)
 
-connection = sqlite3.connect("users.db")
+# connection = sqlite3.connect("users.db")
 
-cursor = connection.cursor()
+# cursor = connection.cursor()
 # REWRITE
 # cursor.execute(
 #    "INSERT INTO users VALUES ('waxx567', 'waynem567@gmail.com', '99 Main St', 'Home Town', '6789', '+27791887713')")
@@ -20,10 +20,12 @@ BUILDER_2 = "fifty"
 BUILDER_3 = "ltd"
 YEAR = "2022"
 
+SITE_INFO = [[CLIENT = client]]  # , CLIENT_1 = "gaslit", CLIENT_2 = "catfish", CLIENT_3 = "designs", BUILDER = "fivefiftyfive ltd", BUILDER_1 = "five", BUILDER_2 = "fifty", BUILDER_3 = "ltd", YEAR = "2022"}
+
 
 @app.route("/")
 def home():
-    return render_template("index.html", client=CLIENT, client_1=CLIENT_1, client_2=CLIENT_2, builder=BUILDER, builder_1=BUILDER_1, builder_2=BUILDER_2, builder_3=BUILDER_3, year=YEAR)
+    return render_template("index.html", SITE_INFO)
 
 
 if __name__ == "__main__":
