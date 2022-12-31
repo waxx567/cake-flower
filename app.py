e78dd33b88c23ceca64cdce6d675f2bd6eaab036
waxx567 <waynem567@gmail.com>
2022-12-31 11:08:10 +0200

    Insert many records into db

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ conn = sqlite3.connect("users.db")
 # Create cursor
 c = conn.cursor()
 
-# Insert one record into db
-c.execute("INSERT INTO test VALUES ('Bob', 'de Bilderberg', 5021)")
+# Insert many records into db
+c.execute()
 
 # Commit command
 conn.commit()
