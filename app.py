8f2632713d58fa76df96682d3ca1a423248f8f17
waxx567 <waynem567@gmail.com>
2022-12-31 10:15:55 +0200

    same

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
 import sqlite3
-from flask import Flask, redirect, render_template, request
-
-app = Flask(__name__)
 
 # Connect to database
 conn = sqlite3.connect("users.db")
@@ -9,21 +6,12 @@ conn = sqlite3.connect("users.db")
 # Create a cursor
 c = conn.cursor()
 
-CLIENT = "gaslitcatfish web designs"
-CLIENT_1 = "gaslit"
-CLIENT_2 = "catfish"
-CLIENT_3 = "designs"
-BUILDER = "fivefiftyfive ltd"
-BUILDER_1 = "five"
-BUILDER_2 = "fifty"
-BUILDER_3 = "ltd"
-YEAR = "2022"
-
-
-@app.route("/")
-def home():
-    return render_template("index.html")
-
+# Create table
+c.execute("""CREATE TABLE test (
+    first_name TEXT,
+    last_name TEXT,
+    number INTEGER
+)""")
 
 # Commit command
 conn.commit()
