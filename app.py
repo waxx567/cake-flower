93f24bc53e6f9f2360cbe6c03d7438fa88770f3f
waxx567 <waynem567@gmail.com>
2022-12-31 10:56:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ conn = sqlite3.connect("users.db")
 c = conn.cursor()
 
 # Insert one record into db
-c.execute("INSERT INTO test VALUES ('Hal', 'Bell', 701)")
+c.execute("INSERT INTO test VALUES ('Bob', 'de Bilderberg', 5021)")
 
 # Commit command
 conn.commit()
