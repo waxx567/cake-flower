fe69a26ddb1970cd5c71fccb26e1c0a51fff66ab
waxx567 <waynem567@gmail.com>
2023-01-01 10:18:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ conn = sqlite3.connect("users.db")
 c = conn.cursor()
 
 # Query database
-c.execute("SELECT rowid, * FROM test ORDER BY last_name")
+c.execute("SELECT rowid, * FROM test ORDER BY number")
 
 items = c.fetchall()
 
