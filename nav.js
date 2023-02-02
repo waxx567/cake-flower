35a571f5c1033be5b927db6c423aed0c0a7b1f4f
waxx567 <waynem567@gmail.com>
2023-02-02 12:28:07 +0200

    Update nav.js

--- nav.js
+++ nav.js
@@ -2,7 +2,9 @@ const primaryNav = document.querySelector(".primary-navigation");
 const navToggle = document.querySelector(".mobile-nav-toggle");
 
 navToggle.addEventListener("click", () => {
-    const visibility = nav.getAttribute("data-visible");
+    const visibility = primaryNav.getAttribute("data-visible");
 
-    console.log(visibility);
+    if(visibility === "false") {
+        primaryNav.setAttribute("data-visible", true);
+    }
 });
