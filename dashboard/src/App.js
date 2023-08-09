742fd2193986c24d5036f45991292a31adf65101
waxx567 <waynem567@gmail.com>
2023-08-09 14:35:31 +0200

    Update App.js

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -34,6 +34,18 @@ const App = () => {
               Sidebar
             </div>
           )}
+          {/* Navbar */}
+          <div
+            className={
+              activeMenu
+                ? 'dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  '
+                : 'bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 '
+            }
+          >
+            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full ">
+              Navbar
+            </div>
+          </div>
         </div>
       </BrowserRouter>
     </div>
